303d787f39c6739eb3c938f33814b6fb2e1633f8
Lê Việt Thắng <levietthang1997@gmail.com>
2021-05-28 15:11:49 +0700

    update

---
+++ Home.java
@@ -0,0 +1,10 @@
+public class Home {
+	public void test() {
+		System.out.println("djt con me may");
+	}
+	
+	public static void main(String[] args) {
+		Home home = new Home();
+		home.test();
+	}
+}
